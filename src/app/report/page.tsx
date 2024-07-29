82a65d3449bad822d8e70759db8e53950a69ebdf
Eduardo-CF04 <edu19990402@gmail.com>
2024-07-28 19:03:50 -0500

    Se agrego los componentes para los graficos

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,12 +1,26 @@
-import Image from "next/image";
+import BarChart from "@/components/BarChart"
+import PieChart from "@/components/pieChart"
+import PolarAreaCharts from "@/components/PolarAreaCharts"
+import DoughnutCharts from "@/components/DoughnutChart"
 
 export default function Report() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20 bg-slate-500">
-      <h1 className="text-2xl">
-        Reportes
-      </h1>
+    <div className="container">
+      Chart js
+      <div >
+        <h3>Bar Chart</h3>
+        <BarChart></BarChart>
+        <h3>Pie Chart</h3>
+        <PieChart></PieChart>
+      </div>
 
-    </main>
+      <div>
+        <h3>Polar Area Chart</h3>
+        <PolarAreaCharts></PolarAreaCharts>
+        <h3>Doughnut Chart</h3>
+        <DoughnutCharts></DoughnutCharts>
+      </div>
+
+    </div>
   );
 }
