c90825a3de39394af41332fdbe6dbf6858033c7d
Eduardo-CF04 <edu19990402@gmail.com>
2024-07-31 08:19:31 -0500

    fixed Page inscription

--- src/app/inscription/page.tsx
+++ src/app/inscription/page.tsx
@@ -1,14 +1,19 @@
+"use client"
+
 import React from "react";
-import { Box, Button, Container, Grid, Paper, Select, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, MenuItem, Paper, Select, TextField, Typography } from "@mui/material";
 
 
 export default function Inscription() {
+  const [institution, setInstitution] = React.useState('');
+  const [belongsToInstitution, setBelongsToInstitution] = React.useState('');
+
   return (
     <Container maxWidth="sm">
       <Grid
         container
         direction="column"
-        alingItems="center"
+        alignItems="center"
         justifyContent="center"
         sx={{ minHeight: "100vh" }}
       >
@@ -47,15 +52,25 @@ export default function Inscription() {
                 fullWidth label="Fecha de nacimiento"
                 sx={{ mt: 1.5, mb: 1.5 }} />
               <Typography sx={{ mt: 1, mb: 1 }} variant="h5">¿Perteneces a alguna institución?</Typography>
-              <Select fullWidth sx={{ mt: 1, mb: 2 }} >
-                <option value="Si">Si</option>
-                <option value="No">No</option>
+              <Select
+                value={belongsToInstitution}
+                onChange={(e) => setBelongsToInstitution(e.target.value)}
+                fullWidth
+                sx={{ mt: 1, mb: 2 }}
+              >
+                <MenuItem value="Yes">Sí</MenuItem>
+                <MenuItem value="No">No</MenuItem>
               </Select>
-              <Typography sx={{ mt: 1, mb: 1 }} variant="h5">Indica la institución que perteneces: </Typography>
-              <Select fullWidth sx={{ mt: 1, mb: 2 }} >
-                <option value="1">Colegio</option>
-                <option value="2">Parroquia</option>
-                <option value="3">Congregación</option>
+              <Typography sx={{ mt: 1, mb: 1 }} variant="h5">Indica la institución a la que perteneces:</Typography>
+              <Select
+                value={institution}
+                onChange={(e) => setInstitution(e.target.value)}
+                fullWidth
+                sx={{ mt: 1, mb: 2 }}
+              >
+                <MenuItem value="Colegio">Colegio</MenuItem>
+                <MenuItem value="Parroquia">Parroquia</MenuItem>
+                <MenuItem value="Congregación">Congregación</MenuItem>
               </Select>
               <Button fullWidth type="submit" variant="contained" sx={{ mt: 1.5, mb: 3 }}>
                 Registrar Datos
